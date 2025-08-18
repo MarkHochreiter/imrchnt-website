a68be79867d52ccdde0307aaf5639e785918984c
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-08-18 10:48:33 -0400

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+    allowedHosts: ['5173-it946u1yi4ojbozc2zu50-e20a7357.manusvm.computer', 'localhost']
+  }
+})
