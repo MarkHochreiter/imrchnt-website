39294bd78f9b8d67315d2b52703de721bfca0724
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-08-19 20:25:47 -0400

    Update SignupModal.jsx

--- src/components/SignupModal.jsx
+++ src/components/SignupModal.jsx
@@ -28,7 +28,7 @@ const SignupModal = ({ isOpen, onClose }) => {
     setErrorDetails('')
 
     // Replace this with your actual Google Apps Script URL
-    const scriptUrl = 'YOUR_GOOGLE_APPS_SCRIPT_URL'
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbyx2JTK79rHqO_hqAWTyw6dLp5GOyuYYiDFiEDPAVhrgDjZ30XiyAy94WpqDJpV2VFKDg/exec'
     
     console.log('=== FORM SUBMISSION DEBUG ===')
     console.log('Script URL:', scriptUrl)
