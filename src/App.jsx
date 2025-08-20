152629ffc1aeffacc9f022f52cdf56e40d50373d
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-08-20 09:29:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ function App() {
               Seamless POS. Intuitive Design.
             </h1>
             <p className="text-xl md:text-2xl text-gray-300 mb-8 max-w-2xl">
-              Pop up, Off site, In store. <br /> Network connection or not.<br /> Minimal training. <br /> Anyone, anywhere, anytime.
+              Pop up, Off site, In store. Network connection or not.<br /> Minimal training. Anyone, anywhere, anytime.
             </p>
             <div className="flex flex-col sm:flex-row gap-4">
               <Button 
