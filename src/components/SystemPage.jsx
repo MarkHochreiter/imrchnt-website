a1eacfb8da2b1e613824840b4fc7418f01d58f64
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-08-21 21:26:02 -0400

    Update SystemPage.jsx

--- src/components/SystemPage.jsx
+++ src/components/SystemPage.jsx
@@ -59,8 +59,10 @@ function SystemPage() {
                   <span>Multi-location support</span>
                 </div>
               </div>
-              <Button className="bg-[#f08e80] hover:bg-[#e07d70] text-white">
+              <Button 
+                className="bg-[#f08e80] hover:bg-[#e07d70] text-white"
                  onClick={() => onNavigate('pos-diagram')}
+                >
                 Explore POS System
               </Button>
             </div>
