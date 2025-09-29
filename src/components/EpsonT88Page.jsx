9b7d37bea47a620bb7e0f77959336bb92d90a2fb
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-09-29 15:45:18 -0400

    Update EpsonT88Page.jsx

--- src/components/EpsonT88Page.jsx
+++ src/components/EpsonT88Page.jsx
@@ -247,8 +247,9 @@ function EpsonT88Page({ onNavigateBack, onContactSales }) {
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
            <Button 
-                className="w-full bg-[#f08e80] hover:bg-[#e07d70] text-white"
-                onClick={onContactSales}
+                  size="lg" 
+                  className="bg-white text-[#f08e80] hover:bg-gray-100 text-lg px-8 py-4"
+                  onClick={onContactSales}
               >
                 Generate Quote
               </Button>
