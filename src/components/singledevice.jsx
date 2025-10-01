df394117bb384ed9c5a2a6ee21b3deb57a5e0d8e
MarkHochreiter <42657078+MarkHochreiter@users.noreply.github.com>
2025-10-01 13:40:28 -0400

    Update singledevice.jsx

--- src/components/singledevice.jsx
+++ src/components/singledevice.jsx
@@ -255,7 +255,7 @@ function SingleDevicePage({ onNavigateBack }) {
         <section className="py-10 mb-12">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
             <h1 className="text-4xl md:text-6xl font-extrabold text-gray-900 mb-6">
-             Offsites, events, pop ups with on piece of hardware.
+             Offsites, events, pop ups with one piece of hardware.
             </h1>
           </div>
         </section>
